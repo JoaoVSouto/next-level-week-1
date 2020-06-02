2846dc3c405ba1b75aa379a81b38bbf4dd9855cb
JoaoVSouto <joaovitorss99@hotmail.com>
2020-06-02 11:56:40 -0300

    [BACKEND] Adds route to list items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 
+import knex from './database/connection';
+
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  const users = ['Diego', 'Cleiton', 'Robson', 'Daniel'];
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
 
-  return res.json(users);
+  const serializedItems = items.map(item => ({
+    ...item,
+    image_url: `http://localhost:3333/uploads/${item.image}`,
+  }));
+
+  return res.json(serializedItems);
 });
 
 export default routes;
