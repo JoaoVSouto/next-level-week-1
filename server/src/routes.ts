7610e0f9f865517bcd06834ea0adc9a9e5a1ba77
JoaoVSouto <joaovitorss99@hotmail.com>
2020-06-02 17:43:57 -0300

    [BACKEND] Uncouples routes handlers to controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,55 +1,12 @@
 import { Router } from 'express';
 
-import knex from './database/connection';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = Router();
 
-routes.get('/items', async (req, res) => {
-  const items = await knex('items').select('*');
+routes.get('/items', ItemsController.index);
 
-  const serializedItems = items.map(item => ({
-    ...item,
-    image_url: `http://localhost:3333/uploads/${item.image}`,
-  }));
-
-  return res.json(serializedItems);
-});
-
-routes.post('/points', async (req, res) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items,
-  } = req.body;
-
-  const trx = await knex.transaction();
-
-  const insertedIds = await trx('points').insert({
-    image: 'placeholder',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-  });
-
-  const [point_id] = insertedIds;
-
-  const pointItems = items.map((item_id: number) => ({
-    item_id,
-    point_id,
-  }));
-
-  await trx('point_items').insert(pointItems);
-
-  return res.json({ success: true });
-});
+routes.post('/points', PointsController.create);
 
 export default routes;
