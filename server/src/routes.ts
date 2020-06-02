fdd380d257947e72207e50c0e8479c1f5fea5139
JoaoVSouto <joaovitorss99@hotmail.com>
2020-06-02 12:16:45 -0300

    [BACKEND] Adds route to create point

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -15,4 +15,41 @@ routes.get('/items', async (req, res) => {
   return res.json(serializedItems);
 });
 
+routes.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = req.body;
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
+    image: 'placeholder',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const [point_id] = insertedIds;
+
+  const pointItems = items.map((item_id: number) => ({
+    item_id,
+    point_id,
+  }));
+
+  await trx('point_items').insert(pointItems);
+
+  return res.json({ success: true });
+});
+
 export default routes;
