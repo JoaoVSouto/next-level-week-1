f17608a4f30512463fc5886d718738b98a4031fa
JoaoVSouto <joaovitorss99@hotmail.com>
2020-06-03 18:25:09 -0300

    [FRONTEND] Adds Leaflet map into CreatePoint

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
@@ -51,6 +52,15 @@ const CreatePoint: React.FC = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[-5.839974, -35.2115434]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-5.839974, -35.2115434]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
