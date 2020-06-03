b63548c5ff1c19a811900b14be7c190f14685e8e
JoaoVSouto <joaovitorss99@hotmail.com>
2020-06-03 17:29:39 -0300

    [FRONTEND] Creates Home page structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header';
+import Home from './pages/Home';
 
 export default function App(): JSX.Element {
-  return (
-    <div className="App">
-      <Header title="Hello world" />
-      <h1>App content</h1>
-    </div>
-  );
+  return <Home />;
 }
