afdb42cc84c61f3f89a396a9b9f2949aaf94be2d
JoaoVSouto <joaovitorss99@hotmail.com>
2020-06-05 18:59:34 -0300

    [WEB] Implements dropzone to send images

---
+++ web/src/components/Dropzone/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { FiUpload } from 'react-icons/fi';
+
+import './styles.css';
+
+const Dropzone: React.FC = () => {
+  const [selectedFileUrl, setSelectedFileUrl] = useState('');
+
+  const onDrop = useCallback(acceptedFiles => {
+    const [file] = acceptedFiles;
+
+    const fileUrl = URL.createObjectURL(file);
+
+    setSelectedFileUrl(fileUrl);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: 'image/*',
+  });
+
+  return (
+    <div className="dropzone" {...getRootProps()}>
+      <input {...getInputProps()} accept="image/*" />
+
+      {selectedFileUrl ? (
+        <img src={selectedFileUrl} alt="Imagem enviada" />
+      ) : (
+        <p>
+          <FiUpload />
+          Imagem do estabelecimento
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Dropzone;
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -7,6 +7,8 @@ import { LeafletMouseEvent } from 'leaflet';
 
 import api from '../../services/api';
 
+import Dropzone from '../../components/Dropzone';
+
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
@@ -160,6 +162,8 @@ const CreatePoint: React.FC = () => {
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
+
+        <Dropzone />
 
         <fieldset>
           <legend>
